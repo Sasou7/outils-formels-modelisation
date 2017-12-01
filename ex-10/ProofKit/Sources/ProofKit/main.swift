ba5aa73ce7a98d2ecb246a5d738ce93c5ac00eea
Sadek <Sadek.Helbawi@etu.unige.ch>
2017-12-01 13:42:19 +0100

    MAJ

---
+++ ex-10/ProofKit/Sources/ProofKit/main.swift
@@ -0,0 +1,55 @@
+import ProofKitLib
+
+let a: Formula = "a"
+let b: Formula = "b"
+let c: Formula = "c"
+let d: Formula = "d"
+let f = a => b , a => c , b => d , c => d |- a => d
+
+print(f.isProvable)
+/*
+let booleanEvaluation = f.eval { (proposition) -> Bool in
+    switch proposition {
+        case "p": return true
+        case "q": return false
+        default : return false
+    }
+}
+print(booleanEvaluation)
+
+enum Fruit: BooleanAlgebra {
+
+    case apple, orange
+
+    static prefix func !(operand: Fruit) -> Fruit {
+        switch operand {
+        case .apple : return .orange
+        case .orange: return .apple
+        }
+    }
+
+    static func ||(lhs: Fruit, rhs: @autoclosure () throws -> Fruit) rethrows -> Fruit {
+        switch (lhs, try rhs()) {
+        case (.orange, .orange): return .orange
+        case (_ , _)           : return .apple
+        }
+    }
+
+    static func &&(lhs: Fruit, rhs: @autoclosure () throws -> Fruit) rethrows -> Fruit {
+        switch (lhs, try rhs()) {
+        case (.apple , .apple): return .apple
+        case (_, _)           : return .orange
+        }
+    }
+
+}
+
+let fruityEvaluation = f.eval { (proposition) -> Fruit in
+    switch proposition {
+        case "p": return .apple
+        case "q": return .orange
+        default : return .orange
+    }
+}
+print(fruityEvaluation)
+*/
